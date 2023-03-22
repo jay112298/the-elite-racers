b4c7381fec8fdaed40e26af92c4aed628615c13e
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-03-22 16:17:11 +0530

    Add Launching soon screen on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import eliteLogo from '/the-elite-racers.svg'
 import './App.css'
 
 function App() {
@@ -10,23 +10,17 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={eliteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>The Elite Racers</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Launching Soon {/*{count} */}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Stay tuned to learn more
       </p>
     </div>
   )
