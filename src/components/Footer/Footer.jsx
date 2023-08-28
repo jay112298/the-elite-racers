d518a0061667780b32c42d82a649009222951008
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-08-29 00:27:08 +0530

    Add footer sections and styling for both Nav and Vision section. Social icons to be done next

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,62 @@
 import React from "react";
 import { FooterSec } from "./Footer.st";
+import footerLogo from "../../assets/elite-footer.svg"
 
 const Footer = () => {
   return (
     <FooterSec>
-        <div className="top"></div>
-        <div className="bottom"></div>
+      <div className="blank"></div>
+      <div className="content">
+        <div className="top">
+          <div className="identity"><img src={footerLogo} alt="Logo" /><div className="name">The Elite <span>Racers</span></div></div>
+          <div className="link"><span>visit: </span>www.theeliteracers.com</div>
+        </div>
+        <div className="bottom">
+          <div className="nav">
+            <div className="left">
+              <div className="footnav-title">Navigation</div>
+              <div className="footnav-content">
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Team</li>
+                  <li>Races</li>
+                  <li>Vehicle</li>
+                  <li>Contact</li>
+                  <li>Recruitments</li>
+                  <li>Partners</li>
+                </ul>
+              </div>
+            </div>
+            <div className="right">
+              <div className="footnav-title">Information</div>
+              <div className="footnav-content">
+                <ul>
+                  <li>Contact</li>
+                  <li>Gallery</li>
+                  <li>Terms of Use</li>
+                  <li>Policies</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="vision">
+            <div className="footnav-title">Our Vision</div>
+            <div className="footnav-content">
+              <div className="para">With a fierance ambition to change the game, The Elite Racers Formula One SAE team of SBJITMR is bringing new energy to F1 in its quest for national level titles. Based in Nagpur, the team's talented driver squad includes skilled drivers as well as assembly technicians inorder to build skilled undergrads for motorsports.</div>
+              <div className="slogan">
+                <div className="shape"></div>
+                <div className="text">Want to <span>join</span> our league?</div>
+                </div>
+              <div className="cta">See Recruitments</div>
+            </div>
+          </div>
+        </div>
+        <div className="social-icons">
+          
+        </div>
+      </div>
+      <div className="blank"></div>
     </FooterSec>
   );
 };
--- src/components/Footer/Footer.st.js
+++ src/components/Footer/Footer.st.js
@@ -2,6 +2,186 @@ import styled from 'styled-components'
 
 export const FooterSec = styled.footer`
 
+    display: flex;
+    align-items: flex-end;
 
+    .blank {
+        width: 4.69577rem;
+        height: 1.98413rem;
+        background: #D9D9D9;
+    }
+
+    .content {
+        width: calc(100rem - 4.69577rem - 4.69577rem);
+
+        .top {
+
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-bottom: 2.12rem;
+            border-bottom: 1.5px solid black;
+
+            .identity {
+                display: flex;
+                align-items: center;
+                width: 33.86243rem;
+                height: 15.47619rem;
+
+                .name{
+                    width: 17.39418rem;
+                    height: 5.09259rem;
+                    color: #000201;
+                    font-family: 'Rugen', sans-serif;
+                    font-size: 2.1545rem;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: normal;
+
+                    span {
+                        color: #D93100;
+                        font-family: 'Rugen', sans-serif;
+                        font-size: 2.84392rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                    }
+                }
+            }
+
+            .link {
+                color: #D93001;
+                font-family: 'Inter', sans-serif;
+                font-size: 1.0582rem;
+                font-style: normal;
+                font-weight: 400;
+                line-height: normal;
+
+                span {
+                    color: #656565;
+                }
+            }
+        }
+
+        .bottom {
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+
+            .nav, .vision{
+                display: flex;
+                align-items: flex-start;
+
+                .footnav-title {
+                    width: 7rem !important;
+                    color: #DD4B21;
+                    font-family: 'Inter', sans-serif;
+                    font-size: 1.0582rem;
+                    font-style: normal;
+                    font-weight: 700;
+                    line-height: normal;
+                    width: 5.5rem;
+                }
+
+                .footnav-content {
+                    li {
+                        list-style: none;
+                        margin-bottom: 0.86rem;
+                        color: #000;
+                        font-family: 'Inter', sans-serif;
+                        font-size: 1.0582rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                    }
+                }
+            }
+
+            .nav {
+                .left, .right {
+                    display: flex;
+                    align-items: flex-start;
+                    justify-content: space-between;
+
+                    
+                }
+            }
+
+            .vision {
+                width: 41.99735rem;
+
+                align-items: flex-start;
+
+                .footnav-title {
+                    width: 30rem !important;
+                }
+
+                .footnav-content {
+
+                    margin-top: 2.05rem;
+                    
+                    .para {
+                        color: #000;
+                        font-family: 'Inter', sans-serif;
+                        font-size: 1.0582rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                        margin-bottom: 1.5rem;
+                    }
+
+                    .slogan {
+
+                        display: flex;
+                        align-items: center;
+                        gap: 0.79rem;
+                        margin-bottom: 0.98rem;
+                        .shape {
+                            width: 0.46296rem;
+                            height: 5.42328rem;
+                            border-radius: 5.29101rem;
+                            background: #DD4B21;
+                        }
+
+                        .text {
+                            width: 26.5873rem;
+                            color: #000;
+                            font-family: 'Inter', sans-serif;
+                            font-size: 2.31481rem;
+                            font-style: normal;
+                            font-weight: 900;
+                            line-height: normal;
+                            text-transform: uppercase;
+
+                            span {
+                                color: #D93001;
+                            }
+                        }
+
+
+
+                    }
+
+                    .cta {
+                        margin-top: 0.93rem;
+                        width: 14.21958rem;
+                        height: 3.24074rem;
+                        border-radius: 0.19841rem;
+                        background: #D93001;
+                        color: #FFF;
+                        font-size: 0.99206rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                        font-family: 'Inter', sans-serif;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                }
+
+            }
+        }
+    }
 
 `
