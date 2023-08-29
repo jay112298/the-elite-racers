776ffec9a3645c49511e5343a277ba755ff270d3
Jitendra Asole <jasole1111@gmail.com>
2023-08-29 11:49:14 +0530

    Create and style all sections on the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Form } from './Login.st'
 import LoginImg from "../../assets/login.png"
+import { Link } from 'react-router-dom'
+import google from "../../assets/google.svg"
+import terSilver from "../../assets/ter-silver.png"
 
 const Login = () => {
   return (
     <Form logincover={LoginImg}>
         <div className="content-block">
-            <div className="img"></div>
-            <div className="form">
-                <div className="title">Hello, Welcome Back!</div>
-                <div className="label">Email</div>
-                <input type="text" />
-                <div className="label">Password</div>
-                <input type="text" />
+            <div className="img">
+              <div className="logo">
+                <img src={terSilver} alt="Logo-Silver" />
+              </div>
+              <div className="slogan">race like you mean it.</div>
+            </div>
+            <div className="form-section">
+                <div className='form-area'>
+                  <div className="title">Hello, Welcome Back!</div>
+                  <div className="label">Email</div>
+                  <input type="text" />
+                  <div className="label">Password</div>
+                  <input type="password" />
+                  <button>Log In</button>
+                  <div className="forgot-pass"><Link to="/forgot-password">Forgot Password?</Link></div>
+                  <div className="or-sec">
+                    <div className='dash'></div>
+                    <div>or</div>
+                    <div className='dash'></div>
+                  </div>
+                  <div className="google-link">
+                    <div className="google"><img src={google} alt="" /></div>
+                    <div className="text">Continue with Google</div>
+                  </div>
+                  <div className="signup-link">Don't Have An Account? <Link>Sign Up</Link></div>
+                </div>
             </div>
         </div>
     </Form>
--- src/pages/Login/Login.st.js
+++ src/pages/Login/Login.st.js
@@ -27,36 +27,161 @@ export const Form = styled.div`
             background-position: center center;
             background-size: cover;
 
+            .logo {
+
+                img {
+                    margin: 1.92rem 1.92rem;
+                    height: 6rem;
+                    width: 6rem;
+                }
+            }
+
+            .slogan {
+                text-transform: uppercase;
+                width: 24.27249rem;
+                height: 6.48148rem;
+                font-family: 'Rugen', sans-serif;
+                font-size: 2.22665rem;
+                font-style: normal;
+                font-weight: 400;
+                line-height: normal;
+                background: linear-gradient(81deg, #FFF 0%, rgba(255, 255, 255, 0.00) 100%);
+                background-clip: text;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                margin-left: 8rem;
+            }
+
         }
 
-        .form {
+        .form-section {
             
             font-family: 'Inter', sans-serif;
 
+            .form-area {
 
-            .title {
-                color: #181818;
-                font-size: 2.24868rem;
-                font-style: normal;
-                font-weight: 700;
-                line-height: normal;
                 margin: 0 6.8rem;
-            }
 
-            .label {
-                color: #17171E;
-                font-size: 0.79365rem;
-                font-style: normal;
-                font-weight: 700;
-                line-height: normal;
-            }
+                .title {
+                    color: #181818;
+                    font-size: 2.24868rem;
+                    font-style: normal;
+                    font-weight: 700;
+                    line-height: normal;
+                }
+    
+                .label {
+                    color: #17171E;
+                    margin-top: 1.8rem;
+                    margin-bottom: 0.4rem;
+                    font-size: 0.79365rem;
+                    font-style: normal;
+                    font-weight: 700;
+                    line-height: normal;
+                }
+    
+                input[type="text"], input[type="password"] {
+                    padding: 0.2rem 1rem;
+                    width: 21.47884rem;
+                    height: 1.65026rem;
+                    border-radius: 0.39683rem;
+                    border: 1px solid #A1A7B0;
+                    background: #FFF;
+                }
+    
+                button {
+                    display: block;
+                    width: 100%;
+                    font-family: 'Inter', sans-serif;
+                    margin: 1.98rem 0;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    /* width: 23.47884rem; */
+                    height: 2.1164rem;
+                    border-radius: 0.33069rem;
+                    background: #D93100;
+                    color: #FFF;
+                    font-size: 0.72751rem;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: normal;
+                    border-color: #D93100;
+                }
 
-            input[type="text"] {
+                .forgot-pass {
+                    margin-top: 0.86rem;
+                    margin-bottom: 1.52rem;
 
-            }
+                    a {
+                        color: #D93100;
 
-            button {
+                        text-decoration: none;
+                    }
+                }
 
+                .or-sec {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin-bottom: 1.52rem;
+
+                    .dash {
+                        width: 10.31746rem;
+                        height: 0.06614rem;
+                        background: #17171E;
+                    }
+                }
+
+                .google-link {
+                    height: 2.1164rem;
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    border-radius: 0.5291rem;
+                    border: 1px solid #A1A7B0;
+                    background: #FDFDFD;
+
+                    .google {
+
+                        height: 1.6rem;
+                        img {
+                            width: 1.6rem;
+                            height: 1.6rem;
+                            margin-right: 0.2rem;
+
+                        }
+                    }
+
+                    .text {
+                        height: 1.2rem;
+                        color: #17171E;
+                        font-size: 1rem;
+                        font-style: normal;
+                        font-weight: 700;
+                        line-height: normal;
+                    }
+                }
+
+                .signup-link {
+                    margin: 0.94rem 0;
+                    font-family: 'Inter', sans-serif;
+                    color: #000;
+                    font-size: 0.72751rem;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: normal;
+
+                    a {
+                        color: #D93100;
+                        font-family: 'Inter', sans-serif;
+                        font-size: 0.72751rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                    }
+                }
             }
         }
     }
