3c0d419cd4e3173f48c684aff844bf3e7554ad86
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-08-31 13:09:18 +0530

    Create and Finish Races Section part 1 according UI Plan V1

--- src/components/Races/Races.jsx
+++ src/components/Races/Races.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Race } from './Races.st'
 import LogoSilver from "../../assets/ter-silver.png"
+import F12021 from "../../assets/home/f1-2021.png"
+import DateShape from "../../assets/home/date-shape.svg"
 
 const Races = () => {
     const images = {
         one: "https://i.ytimg.com/vi/dKtKxy8zJC4/maxresdefault.jpg"
     }
   return (
-    <Race backgroundImage={images.one}>
+    <Race backgroundImage={images.one} f12021={F12021}>
         <div className="header">
             <div className="shape"></div>
             <div className="title">Races</div>
@@ -15,8 +17,10 @@ const Races = () => {
         <div className="divider"></div>
         <div className="info-section">
             <div className="calendar">
-                <div className="text">2023<br /><span>Calendar</span></div>
-                <div className="logo"><img src={LogoSilver} alt="Silver Logo TER" /></div>
+                <div className="car-wrapper">
+                    <div className="text">2023<br /><span>Calendar</span></div>
+                    <div className="logo"><img src={LogoSilver} alt="Silver Logo TER" /></div>
+                </div>
             </div>
             <div className="venue">
                 <div className="header">Upcoming <span>Event</span></div>
@@ -26,8 +30,22 @@ const Races = () => {
                 <div className="footer"><span>FSAE</span> India 2024</div>
             </div>
             <div className="dates">
-                <div className="start"></div>
-                <div className="end"></div>
+                <div className="start">
+                    <div className="text">From</div>
+                    <div className="date">
+                        <div className="num">8</div>
+                        <div className="shape"><img src={DateShape} alt="Shape Drift" /></div>
+                    </div>
+                    <div className="month">July</div>
+                </div>
+                <div className="end">
+                    <div className="text">Ends <span>On</span></div>
+                    <div className="date">
+                        <div className="num">10</div>
+                        <div className="shape"><img src={DateShape} alt="Shape Drift" /></div>
+                    </div>
+                    <div className="month">July</div>
+                </div>
             </div>
         </div>
     </Race>
--- src/components/Races/Races.st.js
+++ src/components/Races/Races.st.js
@@ -41,47 +41,58 @@ export const Race = styled.section`
         gap: 0.66rem;
 
         .calendar {
-            display: flex;
-            justify-content: space-between;
-            flex-direction: column;
-            width: 50.33069rem;
-            height: 19.90741rem;
+
             border-radius: 1.38889rem;
             background: linear-gradient(90deg, #D93100 0%, #000 47.40%, #D93100 78.39%);
             box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-
-            .text {
-                margin: 0.53rem 1.59rem;
-                color: #FFF;
-                font-family: 'Rugen', sans-serif;
-                font-size: 2.05026rem;
-                font-style: normal;
-                font-weight: 400;
-                line-height: normal;
-
-                span {
+            
+            .car-wrapper {
+                display: flex;
+                justify-content: space-between;
+                flex-direction: column;
+                width: 50.33069rem;
+                height: 19.90741rem;
+                background: url(${props => props.f12021});
+                background-size: 70%;
+                background-repeat: no-repeat;
+                background-position: 40% 60%;
+                
+                .text {
+                    margin: 0.53rem 1.59rem;
+                    color: #FFF;
                     font-family: 'Rugen', sans-serif;
-                    font-size: 2.1164rem;
+                    font-size: 2.05026rem;
                     font-style: normal;
                     font-weight: 400;
                     line-height: normal;
-                    letter-spacing: 0.38095rem;
+
+                    span {
+                        font-family: 'Rugen', sans-serif;
+                        font-size: 2.1164rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                        letter-spacing: 0.38095rem;
+                    }
                 }
-            }
 
-            .logo {
-                width: 3.57143rem;
-                height: 1.78571rem;
-                align-self: flex-end;
-                margin: 1.32rem;
-                display: flex;
-                align-items: center;
-                justify-content: center;
 
-                img {
+                .logo {
                     width: 3.57143rem;
+                    height: 1.78571rem;
+                    align-self: flex-end;
+                    margin: 1.32rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+
+                    img {
+                        width: 3.57143rem;
+                    }
                 }
             }
+
+
         }
 
         .venue {
@@ -143,9 +154,55 @@ export const Race = styled.section`
                 border-radius: 0.79365rem;
                 background: #FFF;
                 box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+                display: flex;
+                flex-direction: column;
+
+                .text{
+                    color: #000;
+                    font-family: 'Rugen', sans-serif;
+                    font-size: 0.92593rem;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: normal;
+                    margin-top: 1.39rem;
+                    margin-left: 1.06rem;
+                }
+                .date{
+
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin: 0 0.6rem;
+                    .num{
+                        color: #000;
+                        font-family: 'Rugen', sans-serif;
+                        font-size: 3.83598rem;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: normal;
+                    }
+                    .shape{}
+                }
+                .month{
+                    color: #D93100;
+                    font-family: 'Rugen', sans-serif;
+                    font-size: 1.7756rem;
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: normal;
+                    margin-left: 0.79rem;
+                    margin-bottom: 1.2rem;
+                }
             }
 
             .end {
+
+                .text {
+
+                    span {
+                        color: #D93100;
+                    }
+                }
 
             }
         }
